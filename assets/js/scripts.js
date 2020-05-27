aa72a45937496af6f2890bbe91b9f26aab185907
VAhola <veera.ahola@signify.com>
2020-05-27 21:03:36 +0000

    adding js click on suburbs and related css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,10 @@
-$("BtnSuburbs").one("click", function() {
-    $(".BtnCity").replaceWith("<div class=suburb_first><p>Congratulations for having taken the first step! Get ready to explore the suburbs with us..</p></div>")
-})
+$(".BtnSuburbs").one("click", function(){
+     $(".BtnCity").replaceWith("<div class=suburb_first><p>Congratulations for having taken the first step! Get ready to explore the suburbs with us..</p></div>")
+$(".top p").hide();
+  $(".middle h2").replaceWith("<h3>Welcome to the suburbs!</h3>")
+  $("<br><br>").appendTo(".middle");
+  $("<button>hey</button>").addClass("test1").addClass("horsies").appendTo(".middle");
+  $("<button>hola</button>").addClass("test1").addClass("hiking").appendTo(".middle");
+  $("<button>hello</button>").addClass("test1").addClass("mansion").appendTo(".middle");
+  $("<button>moi</button>").addClass("test1").addClass("cricket").appendTo(".middle");
+});

e3aefee05a92442d00b1bb0c5c29713dd1ef4fb7
VAhola <veera.ahola@signify.com>
2020-05-27 20:09:10 +0000

    corrected js file path in index.html added and styled first js line

---
+++ assets/js/scripts.js
@@ -0,0 +1,3 @@
+$("BtnSuburbs").one("click", function() {
+    $(".BtnCity").replaceWith("<div class=suburb_first><p>Congratulations for having taken the first step! Get ready to explore the suburbs with us..</p></div>")
+})
